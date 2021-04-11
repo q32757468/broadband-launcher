bf661793051d1097e42e6ec32e640244e28bdc64
hsl <32757468@qq.com>
2021-04-11 14:52:20 +0800

    fix:修复目录读取问题

--- src/app.ts
+++ src/app.ts
@@ -12,13 +12,12 @@ interface LoginConfig {
 const getConfig = async () => {
   try {
     const rawData = await fs.promises.readFile(
-      path.join(__dirname, "config.json"),
+      path.join(process.cwd(), "config.json"),
       "utf8"
     );
     const config = JSON.parse(rawData);
     return config;
   } catch (error) {
-    console.log("未找到配置文件config.json，请创建该文件并正确填写配置!");
     throw error;
   }
 };
@@ -33,34 +32,35 @@ const loginAttemp = async () => {
 
   try {
     config = await getConfig();
-  } catch (error) {
-    process.exit();
-  }
 
-  try {
-    const { data: resData } = await loginBroadband({
-      username: config.username,
-      password: config.password,
-    });
+    try {
+      const { data: resData } = await loginBroadband({
+        username: config.username,
+        password: config.password,
+      });
 
-    const str = iconv.decode(Buffer.from(resData), "gb2312");
-    const html = iconv.encode(str, "utf8").toString();
+      const str = iconv.decode(Buffer.from(resData), "gb2312");
+      const html = iconv.encode(str, "utf8").toString();
 
-    if (html.includes("信息页")) {
-      console.log("当前为信息页，再次登录中...");
-      setTimeout(() => {
-        loginAttemp();
-      }, random(1000, 2000));
-    } else if (html.includes("认证成功页")) {
-      console.log("登录成功!");
-      delayDestroy(3);
-    } else {
-      console.log("登录失败，未知原因!");
+      if (html.includes("信息页")) {
+        console.log("当前为信息页，再次登录中...");
+        setTimeout(() => {
+          loginAttemp();
+        }, random(1000, 2000));
+      } else if (html.includes("认证成功页")) {
+        console.log("登录成功!");
+        delayDestroy(3);
+      } else {
+        console.log("登录失败，未知原因!");
+        delayDestroy(3);
+      }
+    } catch (error) {
+      console.log("网络连接失败!");
+      console.log("请确保在相应网络内");
       delayDestroy(3);
     }
   } catch (error) {
-    console.log("网络连接失败!");
-    console.log("请确保在相应网络内");
+    console.log("未找到配置文件config.json，请创建该文件并正确填写配置!");
     delayDestroy(3);
   }
 };
